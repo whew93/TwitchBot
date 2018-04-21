232ef6df81e3a6fbfbe199e13560df47b7eb8ee0
Alex Khloptsev <whew93@wgmail.comwshw>
2018-04-22 02:06:25 +0300

    First commit. Empty default project with authentication

---
+++ TwitchBot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwitchBot.Startup))]
+namespace TwitchBot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
